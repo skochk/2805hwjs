4997f672707cf1f747f15eff45297e71e6dcea5e
Sergey <dropmailqwade@gmail.com>
2019-05-31 19:45:29 +0300

    v2

--- scripts/main.js
+++ scripts/main.js
@@ -10,12 +10,26 @@
             document.querySelector(".lighter3").style.backgroundColor = 'red';
             document.querySelector(".lighter1").style.backgroundColor = 'green';
             document.querySelector(".lighter4").style.backgroundColor = 'green';
-            document.querySelector("").style.backgroundColor = 'white';
+            document.querySelector(".ver5").style.backgroundColor ='green';
+            document.querySelector(".ver1").style.backgroundColor ='green';
+            document.querySelector(".ver3").style.backgroundColor ='green';
+            
+            document.querySelector(".ver2").style.backgroundColor ='black';
+            document.querySelector(".ver4").style.backgroundColor ='black';
+
        
         }else{
             document.querySelector(".lighter1").style.backgroundColor = 'red';
             document.querySelector(".lighter4").style.backgroundColor = 'red';
             document.querySelector(".lighter2").style.backgroundColor = 'green';
             document.querySelector(".lighter3").style.backgroundColor = 'green';
+
+            
+            document.querySelector(".ver5").style.backgroundColor ='black';
+            document.querySelector(".ver1").style.backgroundColor ='black';
+            document.querySelector(".ver3").style.backgroundColor ='gteen';
+            
+            document.querySelector(".ver2").style.backgroundColor ='green';
+            document.querySelector(".ver4").style.backgroundColor ='green';
         }
     })
